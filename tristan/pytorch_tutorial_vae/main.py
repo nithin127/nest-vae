2e2f31f4b7edf297b8b03b06eebd14a5f47cdd52
Tristan Deleu <tristan.deleu@gmail.com>
2017-11-23 19:11:14 -0500

    Fix logsumexp

--- tristan/pytorch_tutorial_vae/main.py
+++ tristan/pytorch_tutorial_vae/main.py
@@ -71,26 +71,10 @@ def to_var(x, **kwargs):
         x = x.cuda()
     return Variable(x, **kwargs)
 
-def log_sum_exp(value, dim=None, keepdim=False):
-    """Numerically stable implementation of the operation
-
-    value.exp().sum(dim, keepdim).log()
-    """
-    # TODO: torch.max(value, dim=None) threw an error at time of writing
-    if dim is not None:
-        m, _ = torch.max(value, dim=dim, keepdim=True)
-        value0 = value - m
-        if keepdim is False:
-            m = m.squeeze(dim)
-        return m + torch.log(torch.sum(torch.exp(value0),
-                                       dim=dim, keepdim=keepdim))
-    else:
-        m = torch.max(value)
-        sum_exp = torch.sum(torch.exp(value - m))
-        if isinstance(sum_exp, Number):
-            return m + math.log(sum_exp)
-        else:
-            return m + torch.log(sum_exp)
+def log_sum_exp(value):
+    m = torch.max(value)
+    sum_exp = torch.sum(torch.exp(value - m))
+    return m + torch.log(sum_exp)
 
 output_folder = 'beta-vae'
 if 'SLURM_JOB_ID' in os.environ:
