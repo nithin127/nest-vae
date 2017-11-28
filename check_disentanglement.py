79296bea59178dcc48413be2282c6a4f5757983a
vasisthn <vasisthn@nithin.local>
2017-11-28 06:58:12 -0500

    Minor bugs fixed 8. Viz

--- check_disentanglement.py
+++ check_disentanglement.py
@@ -42,8 +42,9 @@ for i in range(z_dim):
     z = np.random.uniform(-5,5,(z_dim))
     z = np.asarray([list(z)]*n_images)
     z.T[i] = np.linspace(-5,5,n_images)
+    z = Variable(torch.FloatTensor(z))
     z = z.view(n_images, z_dim, 1, 1)
-    reconst_logits = vae.decoder(Variable(z))
+    reconst_logits = vae.decoder(z)
     reconst_grid = torchvision.utils.make_grid(F.sigmoid(reconst_logits).data,
         normalize=True, scale_each=True)
     writer.add_image('beta-vae/feature_{}'.format(i), reconst_grid, epoch)
