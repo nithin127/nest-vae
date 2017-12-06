65bbeca6358ceadfe83191f11c41f6e5d6a95ae2
Tristan Deleu <tristan.deleu@gmail.com>
2017-12-05 21:32:14 -0500

    Remove unused imports

--- tristan/models/dae_dsprites.py
+++ tristan/models/dae_dsprites.py
@@ -1,7 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
 
 def encoder_block(input_filters, output_filters,
           kernel_size=4, stride=2):
