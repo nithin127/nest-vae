63f5ac17a065f69dd6743fa19616a1253076585c
Nithin Vasisth <7644438+nithin127@users.noreply.github.com>
2017-12-15 15:49:59 -0500

    Update mystery-run.sh

--- mystery-run.sh
+++ mystery-run.sh
@@ -6,42 +6,40 @@
 # Activate the environment
 #source activate duckmeagain
 
-export dataset_now=fasion-mnist
-
 # Run the script
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae2 --no-tsne --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae3 --no-tsne --divergence jensen --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae4 --no-tsne --divergence hellinger --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae5 --no-tsne --divergence wasserstein --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae6 --no-tsne --divergence wasserstein --anirudh --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae7 --no-tsne --divergence jensen --beta 0.5 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae8 --no-tsne --divergence jensen --beta 1.5 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae9 --no-tsne --divergence jensen --beta 2.5 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae10 --no-tsne --divergence jensen --beta 10 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae11 --no-tsne --divergence hellinger --beta 500 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae12 --no-tsne --divergence hellinger --beta 1000 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae13 --no-tsne --divergence hellinger --beta 2000 --dataset $dataset_now
-#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae14 --no-tsne --divergence hellinger --beta 3000 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae15 --no-tsne --divergence hellinger --beta 4000 --dataset $dataset_now
-#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae16 --no-tsne --divergence hellinger --beta 5000 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae17 --no-tsne --divergence hellinger --beta 6000 --dataset $dataset_now
-#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae18 --no-tsne --divergence hellinger --beta 7000 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae19 --no-tsne --divergence hellinger --beta 8000 --dataset $dataset_now
-#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae20 --no-tsne --divergence hellinger --beta 9000 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae21 --no-tsne --divergence hellinger --beta 10000 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae22 --no-tsne --divergence hellinger --beta 11000 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae23 --no-tsne --divergence wasserstein --beta 0.5 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae24 --no-tsne --divergence wasserstein --beta 1 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae25 --no-tsne --divergence wasserstein --beta 1.5 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae26 --no-tsne --divergence wasserstein --beta 2.5 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae27 --no-tsne --divergence wasserstein --beta 5 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae28 --no-tsne --divergence wasserstein --beta 10 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae29 --no-tsne --divergence wasserstein --beta 20 --dataset $dataset_now
-#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae30 --no-tsne --divergence wasserstein --beta 30 --dataset $dataset_now
-#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae31 --no-tsne --divergence wasserstein --beta 40 --dataset $dataset_now
-#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae32 --no-tsne --divergence wasserstein --beta 50 --dataset $dataset_now
-#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae33 --no-tsne --divergence wasserstein --beta 60 --dataset $dataset_now
-#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae34 --no-tsne --divergence wasserstein --beta 70 --dataset $dataset_now
-sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae35 --no-tsne --divergence wasserstein --beta 100 --dataset $dataset_now
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae2 --no-tsne --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae3 --no-tsne --divergence jensen --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae4 --no-tsne --divergence hellinger --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae5 --no-tsne --divergence wasserstein --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae6 --no-tsne --divergence wasserstein --anirudh --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae7 --no-tsne --divergence jensen --beta 0.5 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae8 --no-tsne --divergence jensen --beta 1.5 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae9 --no-tsne --divergence jensen --beta 2.5 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae10 --no-tsne --divergence jensen --beta 10 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae11 --no-tsne --divergence hellinger --beta 500 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae12 --no-tsne --divergence hellinger --beta 1000 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae13 --no-tsne --divergence hellinger --beta 2000 --dataset fashion-mnist
+#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae14 --no-tsne --divergence hellinger --beta 3000 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae15 --no-tsne --divergence hellinger --beta 4000 --dataset fashion-mnist
+#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae16 --no-tsne --divergence hellinger --beta 5000 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae17 --no-tsne --divergence hellinger --beta 6000 --dataset fashion-mnist
+#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae18 --no-tsne --divergence hellinger --beta 7000 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae19 --no-tsne --divergence hellinger --beta 8000 --dataset fashion-mnist
+#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae20 --no-tsne --divergence hellinger --beta 9000 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae21 --no-tsne --divergence hellinger --beta 10000 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae22 --no-tsne --divergence hellinger --beta 11000 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae23 --no-tsne --divergence wasserstein --beta 0.5 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae24 --no-tsne --divergence wasserstein --beta 1 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae25 --no-tsne --divergence wasserstein --beta 1.5 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae26 --no-tsne --divergence wasserstein --beta 2.5 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae27 --no-tsne --divergence wasserstein --beta 5 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae28 --no-tsne --divergence wasserstein --beta 10 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae29 --no-tsne --divergence wasserstein --beta 20 --dataset fashion-mnist
+#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae30 --no-tsne --divergence wasserstein --beta 30 --dataset fashion-mnist
+#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae31 --no-tsne --divergence wasserstein --beta 40 --dataset fashion-mnist
+#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae32 --no-tsne --divergence wasserstein --beta 50 --dataset fashion-mnist
+#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae33 --no-tsne --divergence wasserstein --beta 60 --dataset fashion-mnist
+#sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae34 --no-tsne --divergence wasserstein --beta 70 --dataset fashion-mnist
+sbatch --gres=gpu --qos=high --mem=6000 --time=3:00:00 run.sh --output-folder mystery-vae35 --no-tsne --divergence wasserstein --beta 100 --dataset fashion-mnist
 
 $@
